815334203891b6ed5eed28d447c5c83ed82ea6ea
unkown007 <victormatusse00@gmail.com>
2023-06-27 21:57:30 +0000

    added file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+# configure a new web server
+
+exec { 'apt-get -y update && apt-get -y install nginx':
+    path => '/usr/bin:/bin:/usr/sbin/service'
+}
+
+file_line { 'redirect_me':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    line   => '    rewrite ^/redirect_me/?.*$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    after  => 'server_name _;'
+}
+
+file_line { '404_not_found':
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    line   => '    error_page 404 =404 /404.html;',
+    after  => 'rewrite'
+}
+
+file { '/var/www/html/index.html':
+    ensure  => present,
+    content => 'Hello World!'
+}
+
+file { '/var/www/html/404.html':
+    ensure  => present,
+    content => 'Ceci n\'est pas une page'
+}
+
+exec { 'service nginx restart':
+    path => '/usr/bin:/bin:/usr/sbin/service'
+}
