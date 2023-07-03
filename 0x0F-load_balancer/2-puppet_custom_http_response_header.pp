d71b157fa7e2450707e3fcc792ee8ae34542530a
unkown007 <victormatusse00@gmail.com>
2023-07-03 22:02:12 +0000

    added file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,15 +4,13 @@ exec { 'apt-get -y update && apt-get -y install nginx':
     path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     user => 'root'
 }
-
-file_line { 'X-Served-By':
+-> file_line { 'X-Served-By':
     ensure => present,
     path   => '/etc/nginx/sites-available/default',
     line   => "\tadd_header X-Served-By ${hostname};",
     after  => 'server_name _;'
 }
-
-exec { 'service nginx restart':
+-> exec { 'service nginx restart':
     path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     user => 'root'
 }
